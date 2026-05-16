d01ec6d2f3037f4380fe0b0b2174bc0cef182075
ThaumXleb <breadcutter2204@gmail.com>
2026-05-17 01:22:05 +0500

    Initial commit of UnityDroneSim velocity control

---
+++ Assets/Camera/CameraLookAt.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CameraLookAt : MonoBehaviour {
+
+//	public Transform Target;
+	public GameObject Drone;
+
+	public Vector3 offset;
+//	private Vector3 rotation_off;
+
+	void Start() {
+//		offset = transform.position - Drone.transform.position;
+//		rotation_off = transform.rotation - Drone.transform.rotation;
+	}
+	// Update is called once per frame
+	void Update () {
+//		transform.LookAt (Target);
+		transform.position = Drone.transform.position + offset;
+	}
+}
